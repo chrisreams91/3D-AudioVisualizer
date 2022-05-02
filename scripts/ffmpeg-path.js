b131f8806db85aa098627b131eb0d2513e5577dd
Chris Reams <chrisreams91@gmail.com>
2022-05-02 02:47:19 -0500

    please

--- scripts/ffmpeg-path.js
+++ scripts/ffmpeg-path.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 
-const path = require("path");
+// const path = require("path");
+// const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+// const ffmpegDir = path.dirname(ffmpegPath);
+// console.log(`ffmpegDir : ${ffmpegDir}`);
+
 const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
-const ffmpegDir = path.dirname(ffmpegPath);
-console.log(`${ffmpegDir}`);
+const ffmpeg = require("fluent-ffmpeg");
+ffmpeg.setFfmpegPath(ffmpegPath);
+// console.log(`${ffmpegDir}`);
