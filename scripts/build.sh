c41b5f3fd5b35a16b70227dd237354fc74386ef8
Chris Reams <chrisreams91@gmail.com>
2022-05-02 18:46:48 -0500

    back again

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 npm i
 
-npm install --save @ffmpeg-installer/ffmpeg
+apk update && apk add ffmpeg
+
+# npm install --save @ffmpeg-installer/ffmpeg
 
 # node `./scripts/ffmpeg-path.js`
 
+# export PATH=`./scripts/ffmpeg-path.js`:$PATH
+# echo "PATH: $PATH"
+# ffmpeg 
+# which ffmpeg
 
-export PATH=`./scripts/ffmpeg-path.js`:$PATH
-echo "PATH: $PATH"
-ffmpeg 
-which ffmpeg
-
-FFMPEG_PATH=$(which ffmpeg)
-echo $FFMPEG_PATH
+# FFMPEG_PATH=$(which ffmpeg)
+# echo $FFMPEG_PATH
 
 npm run build
