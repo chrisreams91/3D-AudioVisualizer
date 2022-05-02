dd72db3ee50100e92ef93e29a496184cf9a46aaf
Chris Reams <chrisreams91@gmail.com>
2022-05-02 03:22:03 -0500

    ffs

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,15 +3,15 @@ npm i
 
 npm install --save @ffmpeg-installer/ffmpeg
 
-node `./scripts/ffmpeg-path.js`
+# node `./scripts/ffmpeg-path.js`
 
 
-# export PATH=`./scripts/ffmpeg-path.js`:$PATH
-# echo "PATH: $PATH"
-# ffmpeg 
-# which ffmpeg
+export PATH=`./scripts/ffmpeg-path.js`:$PATH
+echo "PATH: $PATH"
+ffmpeg 
+which ffmpeg
 
-# FFMPEG_PATH=$(which ffmpeg)
-# echo $FFMPEG_PATH
+FFMPEG_PATH=$(which ffmpeg)
+echo $FFMPEG_PATH
 
 npm run build
--- scripts/ffmpeg-path.js
+++ scripts/ffmpeg-path.js
@@ -6,6 +6,6 @@ console.log(`ffmpegPath : ${ffmpegPath}`);
 const ffmpeg = require("fluent-ffmpeg");
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-// const path = require("path");
-// const ffmpegDir = path.dirname(ffmpegPath);
-// console.log(`${ffmpegDir}`);
+const path = require("path");
+const ffmpegDir = path.dirname(ffmpegPath);
+console.log(`${ffmpegDir}`);
