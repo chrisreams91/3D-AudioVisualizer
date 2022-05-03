1a60017b1385602bfd6060e6389620a30ae4b263
Chris Reams <chrisreams91@gmail.com>
2022-05-02 19:00:03 -0500

    ffs

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@
 npm i
 
 npm install --save @ffmpeg-installer/ffmpeg
-export PATH=`src/scripts/ffmpeg-path.js`:$PATH
+export PATH=`./scripts/ffmpeg-path.js`:$PATH
 echo "PATH: $PATH"
 
 which ffmpeg 
