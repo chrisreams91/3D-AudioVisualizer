9c235d8c0b233dc36dd047c70a8b9eeb8326332d
Chris Reams <chrisreams91@gmail.com>
2022-05-02 19:23:00 -0500

    ffs

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
-
-
-#!/bin/sh
 npm i
 
 npm install --save @ffmpeg-installer/ffmpeg
-npm install ffmpeg-static
-
-# export PATH=`./scripts/ffmpeg-path.js`:$PATH
-# echo "PATH: $PATH"
-
-FFMPEG_PATH=$(which ffmpeg)
-echo $FFMPEG_PATH
+export PATH=`./scripts/ffmpeg-path.js`:$PATH
+echo "PATH: $PATH"
 
 ffmpeg
 
--- scripts/ffmpeg-path.js
+++ scripts/ffmpeg-path.js
@@ -1,13 +1,6 @@
 #!/usr/bin/env node
 
-// const path = require("path");
-
-// const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
-// const ffmpeg = require("fluent-ffmpeg");
-// ffmpeg.setFfmpegPath(ffmpegPath);
-
-// const ffmpegDir = path.dirname(ffmpegPath);
-// console.log(`${ffmpegDir}`);
-
-var pathToFfmpeg = require("ffmpeg-static");
-console.log(`${pathToFfmpeg}`);
+const path = require("path");
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const ffmpegDir = path.dirname(ffmpegPath);
+console.log(`${ffmpegDir}`);
