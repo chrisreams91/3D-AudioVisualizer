f0581065fcd5bd754d7dee50b3690c7bf6c87d18
Chris Reams <chrisreams91@gmail.com>
2022-05-06 16:35:29 -0500

    cleanup

--- server-local.js
+++
@@ -1,3 +0,0 @@
-const app = require("./express/server");
-
-app.listen(4000, () => console.log("Local app listening on port 3000!"));
--- webpack.functions.js
+++
@@ -1,6 +0,0 @@
-const nodeExternals = require("webpack-node-externals");
-
-module.exports = {
-  optimization: { minimize: false },
-  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
-};
